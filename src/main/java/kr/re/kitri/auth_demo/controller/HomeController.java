5f15762050bdfd15e77594aa58ff4033de13d04f
jins040 <jins920409@gmail.com>
2017-08-17 16:16:57 +0900

    2017/08/17 강의내용 (Ajax 처리)

--- src/main/java/kr/re/kitri/auth_demo/controller/HomeController.java
+++ src/main/java/kr/re/kitri/auth_demo/controller/HomeController.java
@@ -8,8 +8,11 @@ import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.ResponseBody;
 
 import javax.servlet.http.HttpSession;
+import java.util.ArrayList;
+import java.util.List;
 
 @Controller
 public class HomeController {
@@ -34,6 +37,22 @@ public class HomeController {
         }
 
         return "main";
+    }
+
+    /**
+     *  Ajax 처리
+     */
+    @GetMapping("/products")
+    @ResponseBody               // JSON 처리
+    public List<Member> products() {
+
+        List<Member> members = new ArrayList<>();
+        members.add(new Member("kim", "soongon"));
+        members.add(new Member("park", "chanho"));
+        members.add(new Member("moon", "jaein"));
+        members.add(new Member("kim", "youngsam"));
+
+        return members;
     }
 
     @GetMapping("/viewProducts")
