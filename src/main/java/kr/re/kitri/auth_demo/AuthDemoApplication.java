6af53153eecebd65665b4db1f9ca281a6a143cd0
jins040 <jins920409@gmail.com>
2017-08-17 14:45:11 +0900

    2017/08/17 강의내용 (로그인 Interceptor 처리)

--- src/main/java/kr/re/kitri/auth_demo/AuthDemoApplication.java
+++ src/main/java/kr/re/kitri/auth_demo/AuthDemoApplication.java
@@ -1,12 +1,27 @@
 package kr.re.kitri.auth_demo;
 
+import kr.re.kitri.auth_demo.interceptor.MeasuringInterceptor;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter;
 
 @SpringBootApplication
 public class AuthDemoApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(AuthDemoApplication.class, args);
+	}
+
+	@Bean
+	public WebMvcConfigurerAdapter webMvcConfigurerAdapter() {
+		return new WebMvcConfigurerAdapter() {
+			@Override
+			public void addInterceptors(InterceptorRegistry registry) {
+				registry.addInterceptor(new MeasuringInterceptor()).addPathPatterns("/**");
+				// + 인터셉터 추가하는 순서대로 인터셉터가 걸린다, excludePath 하면 예외 경로도 설정할 수 있다.
+			}
+		};
 	}
 }
--- src/main/java/kr/re/kitri/auth_demo/interceptor/MeasuringInterceptor.java
+++ src/main/java/kr/re/kitri/auth_demo/interceptor/MeasuringInterceptor.java
@@ -1,4 +1,44 @@
 package kr.re.kitri.auth_demo.interceptor;
 
-public class MeasuringInterceptor {
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.stereotype.Component;
+import org.springframework.web.servlet.handler.HandlerInterceptorAdapter;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+@Component
+public class MeasuringInterceptor extends HandlerInterceptorAdapter {
+
+
+    public static Logger log = LoggerFactory.getLogger(MeasuringInterceptor.class);
+
+    // Ctrl + O 해서 상속받을 것 선택
+    // true 일 때 무사 통과
+    @Override
+    public boolean preHandle(HttpServletRequest request,
+                             HttpServletResponse response,
+                             Object handler) throws Exception {
+
+        //log.info("in the preHandle of MeasuringInterceptor");
+        long millis = System.currentTimeMillis();
+        request.setAttribute("b_time", millis);                 // 요청 들어올 때 시작 시간(begin time)
+        return true;
+    }
+
+    @Override
+    public void afterCompletion(HttpServletRequest request,
+                                HttpServletResponse response,
+                                Object handler,
+                                Exception ex) throws Exception {
+
+        //log.info("in the afterCompletion of MeasuringInterceptor");
+        long endMillis = System.currentTimeMillis();                // 응답 완료되었을 때 시간
+        long beginMillis = (long)request.getAttribute("b_time");
+
+        long elapsedTime = endMillis - beginMillis;
+
+        log.info(request.getRequestURI() + "의 실행 시간은 " + elapsedTime + "ms 입니다.");
+    }
 }
